ac9c6bb1e7e4def656a7ed493eb37809b1bdfba6
Prateek Surana <prateeksurana3255@gmail.com>
2025-07-13 15:31:35 +0530

    Update Go module version to 1.24 and refactor main function for JavaScript integration

--- main.go
+++ main.go
@@ -6,27 +6,39 @@ import (
 	"monkey/lexer"
 	"monkey/object"
 	"monkey/parser"
+	"syscall/js"
 )
 
 func main() {
-	input := `1==1;`
+	js.Global().Set("executeNoCap", ExecuteNoCap())
+	<-make(chan struct{}) // Block forever
+}
 
-	l := lexer.New(input)
-	p := parser.New(l)
-	env := object.NewEnvironment()
-
-	program := p.ParseProgram()
-	if len(p.Errors()) != 0 {
-		for _, msg := range p.Errors() {
-			fmt.Println("Parser error:", msg)
+func ExecuteNoCap() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) any {
+		if len(args) != 1 {
+			return "Invalid number of arguments. Expected 1 argument."
 		}
-		return
-	}
 
-	evaluated := evaluator.Eval(program, env)
-	if evaluated != nil {
-		fmt.Println("Evaluated result:", evaluated.Inspect())
-	} else {
-		fmt.Println("No evaluation result.")
-	}
+		input := args[0].String()
+		l := lexer.New(input)
+		p := parser.New(l)
+		env := object.NewEnvironment()
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			errors := make([]string, 0, len(p.Errors()))
+			for _, msg := range p.Errors() {
+				errors = append(errors, msg)
+			}
+			return errors
+		}
+
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			return fmt.Sprintf("Evaluated result: %s", evaluated.Inspect())
+		} else {
+			return "Evaluation returned nil."
+		}
+	})
 }
